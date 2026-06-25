7389ebd0c8000298d40868ea9ebd5980983533bb
Jacob Nagora <nagorajacob@gmail.com>
2026-06-24 22:06:40 -0400

    Added liability stream computation and statistics

--- main.py
+++ main.py
@@ -45,7 +45,10 @@ def calculate_bond_price(bond_tenor, bond_coupon, bond_face_value, coupon_freque
     # Generate the precise timeline array of cash flow dates (e.g., [0.6, 1.6, ..., 10.6])
     cash_flow_times = np.arange(stub, bond_tenor + 0.01, period_length)
 
-    coupon_payment = (bond_coupon / coupon_frequency) * bond_face_value
+    if bond_face_value == 0:
+        coupon_payment = bond_coupon / coupon_frequency
+    else:
+        coupon_payment = (bond_coupon / coupon_frequency) * bond_face_value
     total_price = 0.0
 
     # Loop sequentially through the actual chronological payment times
@@ -71,28 +74,28 @@ bonds = [
         'name': 'Bond_1',
         'bond_tenor': 5,
         'bond_coupon': 0.08,
-        'bond_face_value': 1000.0,
+        'bond_face_value': 100000.0,
         'coupon_frequency': 1
     },
     {
         'name': 'Bond_2',
         'bond_tenor': 10,
         'bond_coupon': 0.07,
-        'bond_face_value': 1000.0,
+        'bond_face_value': 100000.0,
         'coupon_frequency': 1
     },
     {
         'name': 'Bond_3',
         'bond_tenor': 15,
         'bond_coupon': 0.08,
-        'bond_face_value': 1000.0,
+        'bond_face_value': 100000.0,
         'coupon_frequency': 1
     },
     {
         'name': 'Bond_4',
         'bond_tenor': 15,
         'bond_coupon': 0.06,
-        'bond_face_value': 1000.0,
+        'bond_face_value': 100000.0,
         'coupon_frequency': 2
     }
 
@@ -136,10 +139,73 @@ bonds_results = bond_results[['Bond_ID', 'Baseline Yield (Spot Rate)', '2-Year S
 krd_2_year = (bonds_results['2-Year Shock -'] - bonds_results['2-Year Shock +'])/(2 * 0.01 * bonds_results['Baseline Yield (Spot Rate)'])
 krd_5_year = (bonds_results['5-Year Shock -'] - bonds_results['5-Year Shock +'])/(2 * 0.01 * bonds_results['Baseline Yield (Spot Rate)'])
 krd_10_year = (bonds_results['10-Year Shock -'] - bonds_results['10-Year Shock +'])/(2 * 0.01 * bonds_results['Baseline Yield (Spot Rate)'])
-krds = pd.DataFrame({ 'Bond_ID': bonds_results['Bond_ID'], 'KRD_2_Year': krd_2_year, 'KRD_5_Year': krd_5_year, 'KRD_10_Year': krd_10_year})
+bond_krds = pd.DataFrame({ 'Bond_ID': bonds_results['Bond_ID'], 'KRD_2_Year': krd_2_year, 'KRD_5_Year': krd_5_year, 'KRD_10_Year': krd_10_year})
 
-print(krds)
-# # 8.2 Calculate each bond's total duration
+print(bond_krds)
 
+# # 8.2 Calculate each bond's convexity
+convexity_2_year = (bonds_results['2-Year Shock +'] + bonds_results['2-Year Shock -'] - 2 * bonds_results['Baseline Yield (Spot Rate)'])/(0.01**2 * bonds_results['Baseline Yield (Spot Rate)'])
+convexity_5_year = (bonds_results['5-Year Shock +'] + bonds_results['5-Year Shock -'] - 2 * bonds_results['Baseline Yield (Spot Rate)'])/(0.01**2 * bonds_results['Baseline Yield (Spot Rate)'])
+convexity_10_year = (bonds_results['10-Year Shock +'] + bonds_results['10-Year Shock -'] - 2 * bonds_results['Baseline Yield (Spot Rate)'])/(0.01**2 * bonds_results['Baseline Yield (Spot Rate)'])
+bond_convexities = pd.DataFrame({ 'Bond_ID': bonds_results['Bond_ID'], 'Convexity_2_Year': convexity_2_year, 'Convexity_5_Year': convexity_5_year, 'Convexity_10_Year': convexity_10_year})  
 
-# # 8.3 Calculate each bond's convexity
+print(bond_convexities)
+
+# 9. Define static liability stream - Flat annuity
+# Assuming a flat annuity of $100,000 per year for 10 years 
+liabilities = [
+    {
+        'name': 'Liability_1',
+        'tenor': 20,
+        'coupon': 100000.0,
+        'face_value': 0.0,
+        'coupon_frequency': 1
+    }]
+
+# 10. Loop through liabilities and calculate liability PVs
+results = []
+
+for liability in liabilities:
+    liability_results = {'Liability_ID': liability['name']}
+
+    for curve in yield_curves:
+        price = calculate_bond_price(
+            liability['tenor'],
+            liability['coupon'],
+            liability['face_value'],
+            liability['coupon_frequency'],
+            curve
+        )
+        liability_results[curve] = price
+
+    results.append(liability_results)
+print("\n\nLiability Summary:")
+liability_results = pd.DataFrame(results)
+print(liability_results.to_string(index=False))
+# 10. Calculate liability statistics
+## 10.1 Calculate each liability's KRD
+### Convert liability_results to a DataFrame for easier manipulation
+liabilities_results = liability_results[['Liability_ID', 'Baseline Yield (Spot Rate)', '2-Year Shock +', '2-Year Shock -',
+                            '5-Year Shock +', '5-Year Shock -', '10-Year Shock +', '10-Year Shock -']]
+
+### Calculate KRD's for each shock
+krd_2_year = (liabilities_results['2-Year Shock -'] - liabilities_results['2-Year Shock +'])/(2 * 0.01 * liabilities_results['Baseline Yield (Spot Rate)'])
+krd_5_year = (liabilities_results['5-Year Shock -'] - liabilities_results['5-Year Shock +'])/(2 * 0.01 * liabilities_results['Baseline Yield (Spot Rate)'])
+krd_10_year = (liabilities_results['10-Year Shock -'] - liabilities_results['10-Year Shock +'])/(2 * 0.01 * liabilities_results['Baseline Yield (Spot Rate)'])
+bond_krds = pd.DataFrame({ 'Bond_ID': liabilities_results['Liability_ID'], 'KRD_2_Year': krd_2_year, 'KRD_5_Year': krd_5_year, 'KRD_10_Year': krd_10_year})
+
+print(bond_krds)
+
+# # 10.2 Calculate each liability's convexity
+convexity_2_year = (liabilities_results['2-Year Shock +'] + liabilities_results['2-Year Shock -'] - 2 * liabilities_results['Baseline Yield (Spot Rate)'])/(0.01**2 * liabilities_results['Baseline Yield (Spot Rate)'])
+convexity_5_year = (liabilities_results['5-Year Shock +'] + liabilities_results['5-Year Shock -'] - 2 * liabilities_results['Baseline Yield (Spot Rate)'])/(0.01**2 * liabilities_results['Baseline Yield (Spot Rate)'])
+convexity_10_year = (liabilities_results['10-Year Shock +'] + liabilities_results['10-Year Shock -'] - 2 * liabilities_results['Baseline Yield (Spot Rate)'])/(0.01**2 * liabilities_results['Baseline Yield (Spot Rate)'])
+bond_convexities = pd.DataFrame({ 'Bond_ID': liabilities_results['Liability_ID'], 'Convexity_2_Year': convexity_2_year, 'Convexity_5_Year': convexity_5_year, 'Convexity_10_Year': convexity_10_year})  
+
+print(bond_convexities)
+
+# 11. Calculate all key tenor shock rate curves for the portfolio and liability streams
+
+# 12. Calculate all key tenor shock scenarios for the portfolio and liability streams
+
+# 13. Calculate surplus/deficit for each shock scenario
